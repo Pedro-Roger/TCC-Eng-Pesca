09a0aa6be44d305719753ea194c331f789de3816
Pedro-Roger <eng.pedrodev@gmail.com>
2025-01-19 10:47:36 -0300

    input styles changes

---
+++ src/pages/StyledInputs.ts
@@ -0,0 +1,173 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.input`
+  border: none;
+  outline: none;
+  margin-top: 20px;
+  font-size: 14px;
+  padding: 8px;
+  border-radius: 4px;
+  background-color: transparent;
+  color: aliceblue;
+
+  ::placeholder {
+    color: white;
+  }
+
+  @media (max-width: 1400px) {
+    font-size: 13px;
+    padding: 7px;
+  }
+
+  @media (max-width: 1600px) {
+    font-size: 14px;
+    padding: 8px;
+  }
+
+  @media (max-width: 1920px) {
+    font-size: 15px;
+    padding: 10px;
+  }
+
+  @media (max-width: 2560px) {
+    font-size: 16px;
+    padding: 12px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+    padding: 6px;
+  }
+
+  @media (max-width: 480px) {
+    font-size: 10px;
+    padding: 4px;
+    margin-top: 10px;
+  }
+`;
+
+export const StyledBox = styled.div`
+  background-color: #064f57;
+  width: 290px;
+  height: 94px;
+  color: aliceblue;
+  border-radius: 8px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  position: relative;
+
+  @media (max-width: 1400px) {
+    width: 250px;
+    height: 85px;
+  }
+
+  @media (max-width: 1600px) {
+    width: 280px;
+    height: 90px;
+  }
+
+  @media (max-width: 1920px) {
+    width: 300px;
+    height: 100px;
+  }
+
+  @media (max-width: 2560px) {
+    width: 320px;
+    height: 110px;
+  }
+
+  @media (max-width: 768px) {
+    width: 220px;
+    height: 80px;
+  }
+
+  @media (max-width: 480px) {
+    width: 180px;
+    height: 70px;
+  }
+`;
+
+export const IconWrapper = styled.div`
+  margin-left: 30px;
+  margin-top: 25px;
+  margin-right: 40px;
+
+  @media (max-width: 1400px) {
+    margin-left: 25px;
+    margin-top: 20px;
+    margin-right: 35px;
+  }
+
+  @media (max-width: 1600px) {
+    margin-left: 28px;
+    margin-top: 23px;
+    margin-right: 38px;
+  }
+
+  @media (max-width: 1920px) {
+    margin-left: 32px;
+    margin-top: 27px;
+    margin-right: 42px;
+  }
+
+  @media (max-width: 2560px) {
+    margin-left: 35px;
+    margin-top: 30px;
+    margin-right: 45px;
+  }
+
+  @media (max-width: 768px) {
+    margin-left: 20px;
+    margin-top: 20px;
+    margin-right: 30px;
+  }
+
+  @media (max-width: 480px) {
+    margin-left: 15px;
+    margin-top: 15px;
+    margin-right: 20px;
+  }
+`;
+
+export const LabelText = styled.p`
+  position: absolute;
+  top: -55%;
+  left: -50%;
+  transform: translate(64%, 50%);
+  font-size: 14px;
+  white-space: nowrap;
+  text-align: center;
+
+  @media (max-width: 1400px) {
+    font-size: 13px;
+    transform: translate(60%, 50%);
+    
+  }
+
+  @media (max-width: 1600px) {
+    font-size: 14px;
+  }
+
+  @media (max-width: 1920px) {
+    font-size: 15px;
+    transform: translate(65%, 50%);
+    left: -36%;
+  }
+
+  @media (max-width: 2560px) {
+    font-size: 16px;
+    transform: translate(67%, 50%);
+  }
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+    top: -50%;
+    transform: translate(60%, 50%);
+  }
+
+  @media (max-width: 480px) {
+    font-size: 10px;
+    top: -45%;
+    transform: translate(55%, 50%);
+  }
+`;
--- src/pages/planejamento.tsx
+++ src/pages/planejamento.tsx
@@ -1,456 +1,190 @@
-import { Box, Flex, Heading, Input, Text, } from "@chakra-ui/react";
-import  { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GiFlour, GiMountainCave, GiWeight } from "react-icons/gi";
 import { PiShrimpFill } from "react-icons/pi";
 import { FaCalculator, FaClock, FaWeightHanging, FaWeightScale } from "react-icons/fa6";
+import { StyledInput, StyledBox, IconWrapper, LabelText } from "./StyledInputs";
+
 const Planejamento = () => {
+  const [areaVolume, setAreaVolume] = useState<number>(0);
+  const [pesoMedioDesejado, setPesoMedioDesejado] = useState<number>(0);
+  const [pesoTotalDesejado, setPesoTotalDesejado] = useState<number>(0);
+  const [quantidadeAnimais, setQuantidadeAnimais] = useState<number>(0);
+  const [fcaEstimado, setFcaEstimado] = useState<number>(0);
+  const [quantidadeRacao, setQuantidadeRacao] = useState<number>(0);
+  const [quantidadeSacas, setQuantidadeSacas] = useState<number>(0);
+  const [densidade, setDensidade] = useState<number>(0);
 
-    const [areaVolume, setAreaVolume] = useState<number>(0);
-    const [pesoMedioDesejado, setPesoMedioDesejado] = useState<number>(0);
-    const [pesoTotalDesejado, setPesoTotalDesejado] = useState<number>(0);
-    const [quantidadeAnimais, setQuantidadeAnimais] = useState<number>(0);
-    const [fcaEstimado, setFcaEstimado] = useState<number>(0);
-    const [quantidadeRacao, setQuantidadeRacao] = useState<number>(0);
-    const [quantidadeSacas, setQuantidadeSacas] = useState<number>(0);
-    const [densidade, setDensidade] = useState<number>(0);
-  
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    const calcularPlanejamento = () => {
-      
-      const qtdAnimais = pesoTotalDesejado / (pesoMedioDesejado / 1000);
-      setQuantidadeAnimais(qtdAnimais);
-  
-      const qtdRacao = fcaEstimado * pesoTotalDesejado;
-      setQuantidadeRacao(qtdRacao);
-  
-      const qtdSacas = (qtdRacao / 25);
-      setQuantidadeSacas(qtdSacas);
-  
-      const densidadeAnimal = qtdAnimais /areaVolume  ;
-      setDensidade(densidadeAnimal);
-    };
-  
-    
-    useEffect(() => {
-      console.log('Valores de entrada:', areaVolume, pesoMedioDesejado, pesoTotalDesejado, fcaEstimado);
-      
-      if (
-        areaVolume > 0 &&
-        pesoMedioDesejado > 0 &&
-        pesoTotalDesejado > 0 &&
-        fcaEstimado > 0
-      ) {
-        calcularPlanejamento();
-      }
-    }, [areaVolume, pesoMedioDesejado, pesoTotalDesejado, fcaEstimado, calcularPlanejamento]);
-    
-    const handleInputChange = (field: string, value: string) => {
-      const parsedValue = parseFloat(value) || 0;
-  
-      if (field === "areaVolume") {
-        setAreaVolume(parsedValue);
-      } else if (field === "pesoTotalDesejado") {
-        setPesoTotalDesejado(parsedValue);
-      } else if (field === "pesoMedioDesejado") {
-        setPesoMedioDesejado(parsedValue);
-      } else if (field === "fcaEstimado") {
-        setFcaEstimado(parsedValue);
-      }
-    };
+  const calcularPlanejamento = () => {
+    const qtdAnimais = pesoTotalDesejado / (pesoMedioDesejado / 1000);
+    setQuantidadeAnimais(qtdAnimais);
 
-    return ( 
-        <>
-         <Heading as={"h1"} fontSize={"40px"} color={"aliceblue"} fontWeight={"400"} ml={"150px"} mt={"10px"}>
-            Planejamento
-          </Heading>
+    const qtdRacao = fcaEstimado * pesoTotalDesejado;
+    setQuantidadeRacao(qtdRacao);
 
-          <Flex wrap={"wrap"} padding={"100px"} ml={"50px"} gap={"50px"}>
-            <Box bg={"#064F57"} w={"290px"} h={"94px"} color={"aliceblue"} rounded="md" boxShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"} position={"relative"} display={"flex"}>
-              <Box ml={"30px"} mt={"25px"} mr={"40px"}>
-                <GiMountainCave size={40} color={"aliceblue"} />
-              </Box>
-              <Box>
-                <Text position={"absolute"} top={"-55%"} left={"-47%"} transform={"translate(64%, 50%)"}>
-                  Área (m2) ou Volume (L)
-                </Text>
-                <Input
-                  placeholder="Digite aqui"
-                  border={"none"}
-                  type="number"
-                  value={areaVolume}
-                  onChange={(e) => handleInputChange('areaVolume', e.target.value)}
-                  _focus={{ outline: "none", borderColor: "none" }}
-                  mt={"20px"}
-                />
-              </Box>
-            </Box>
+    const qtdSacas = qtdRacao / 25;
+    setQuantidadeSacas(qtdSacas);
 
-            
-            <Box
-              bg={"#064F57"}
-              w={"290px"}
-              h={"94px"}
-              color={"aliceblue"} 
-              rounded="md" 
-              boxShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}
-              position={"relative"}
-              
-              
-              display={"flex"}
-              >
-                <Box
-                  ml={"30px"}
-                  mt={"25px"}
-                  mr={"40px"}
-                  >
-                  
-                    <FaWeightScale size={40} color={"aliceblue"} />
-                </Box>
+    const densidadeAnimal = qtdAnimais / areaVolume;
+    setDensidade(densidadeAnimal);
+  };
 
-                <Box>
-                <Text
-                position={"absolute"}
-                top={"-55%"}
-                left={"-41%"}
-                transform={"translate(64%, 50%)"}
-                  
-                  
-                >
-                  Peso médio desejado
-                </Text>
-                
-                <Input
-                  placeholder="Digite aqui"
-                  border={"none"}
-                  type="number"
-                  value={pesoMedioDesejado}
-                  onChange={(e) => handleInputChange('pesoMedioDesejado', e.target.value)}
-                  _focus={{ outline: "none", borderColor: "none" }}
-                  mt={"20px"}
-                />
-                </Box> 
+  useEffect(() => {
+    if (areaVolume > 0 && pesoMedioDesejado > 0 && pesoTotalDesejado > 0 && fcaEstimado > 0) {
+      calcularPlanejamento();
+    }
+  }, [areaVolume, pesoMedioDesejado, pesoTotalDesejado, fcaEstimado]);
 
-                
-                
-               
-                
-            </Box>
+  const handleInputChange = (field: string, value: string) => {
+    const parsedValue = parseFloat(value) || 0;
 
-            <Box
-              bg={"#064F57"}
-              w={"290px"}
-              h={"94px"}
-              color={"aliceblue"} 
-              rounded="md" 
-              boxShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}
-              position={"relative"}
-              
-              display={"flex"}
-              >
-                <Box
-                  ml={"30px"}
-                  mt={"25px"}
-                  mr={"40px"}
-                  >
-                  
-                    <FaWeightHanging size={40} color={"aliceblue"} />
-                </Box>
+    if (field === "areaVolume") {
+      setAreaVolume(parsedValue);
+    } else if (field === "pesoTotalDesejado") {
+      setPesoTotalDesejado(parsedValue);
+    } else if (field === "pesoMedioDesejado") {
+      setPesoMedioDesejado(parsedValue);
+    } else if (field === "fcaEstimado") {
+      setFcaEstimado(parsedValue);
+    }
+  };
 
-                <Box>
-                <Text
-                position={"absolute"}
-                top={"-55%"}
-                left={"-38%"}
-                transform={"translate(64%, 50%)"}
-                  
-                  
-                >
-                  Peso total desejado
-                </Text>
-                
-                <Input
-                  placeholder="Digite aqui"
-                  border={"none"}
-                  type="number"
-                  value={pesoTotalDesejado}
-                  onChange={(e) => handleInputChange('pesoTotalDesejado', e.target.value)}
-                  _focus={{ outline: "none", borderColor: "none" }}
-                  mt={"20px"}
-                />
-                </Box> 
+  return (
+    <>
+      <h1 style={{ fontSize: "40px", color: "aliceblue", fontWeight: 400, marginLeft: "150px", marginTop: "10px" }}>
+        Planejamento
+      </h1>
 
-                
-                
-               
-                
-            </Box>
+      <div style={{ display: "flex", flexWrap: "wrap", padding: "100px", marginLeft: "50px", gap: "50px" }}>
+        {/* Área ou Volume */}
+        <StyledBox>
+          <IconWrapper>
+            <GiMountainCave size={40} color={"aliceblue"} />
+          </IconWrapper>
+          <div>
+            <LabelText>Área (m2) ou Volume (L)</LabelText>
+            <StyledInput
+              placeholder="Digite aqui"
+              type="number"
+              value={areaVolume}
+              onChange={(e) => handleInputChange("areaVolume", e.target.value)}
+            />
+          </div>
+        </StyledBox>
 
+        {/* Peso Total Desejado */}
+        <StyledBox>
+          <IconWrapper>
+            <FaWeightHanging size={40} color={"aliceblue"} />
+          </IconWrapper>
+          <div>
+            <LabelText>Peso Total Desejado (kg)</LabelText>
+            <StyledInput
+              placeholder="Digite aqui"
+              type="number"
+              value={pesoTotalDesejado}
+              onChange={(e) => handleInputChange("pesoTotalDesejado", e.target.value)}
+            />
+          </div>
+        </StyledBox>
 
-            <Box
-              bg={"#064F57"}
-              w={"290px"}
-              h={"94px"}
-              color={"aliceblue"} 
-              rounded="md" 
-              boxShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}
-              position={"relative"}
-              
-              display={"flex"}
-              >
-                <Box
-                  ml={"30px"}
-                  mt={"25px"}
-                  mr={"40px"}
-                  >
-                  
-                    <PiShrimpFill size={40} color={"aliceblue"} />
-                </Box>
+        {/* Peso Médio Desejado */}
+        <StyledBox>
+          <IconWrapper>
+            <FaWeightScale size={40} color={"aliceblue"} />
+          </IconWrapper>
+          <div>
+            <LabelText>Peso Médio Desejado (g)</LabelText>
+            <StyledInput
+              placeholder="Digite aqui"
+              type="number"
+              value={pesoMedioDesejado}
+              onChange={(e) => handleInputChange("pesoMedioDesejado", e.target.value)}
+            />
+          </div>
+        </StyledBox>
 
-                <Box>
-                <Text
-                position={"absolute"}
-                top={"-55%"}
-                left={"-45%"}
-                transform={"translate(64%, 50%)"}
-                  
-                  
-                >
-                  Quantidade de animais:
-                </Text>
-                
-                <Input
-                  placeholder={quantidadeAnimais ? quantidadeAnimais.toFixed(0)  : ""}
-                  _placeholder={{
-                    color: "white", 
-                  }}
-                  border={"none"}
-                  type="number"
-                  readOnly
-                  _focus={{ outline: "none", borderColor: "none" }}
-                  mt={"20px"}
-                  
-                />
-                </Box> 
+        {/* FCA Estimado */}
+        <StyledBox>
+          <IconWrapper>
+            <FaCalculator size={40} color={"aliceblue"} />
+          </IconWrapper>
+          <div>
+            <LabelText>FCA Estimado</LabelText>
+            <StyledInput
+              placeholder="Digite aqui"
+              type="number"
+              value={fcaEstimado}
+              onChange={(e) => handleInputChange("fcaEstimado", e.target.value)}
+            />
+          </div>
+        </StyledBox>
 
-                
-                
-               
-                
-            </Box>
+        {/* Quantidade de Animais */}
+        <StyledBox>
+          <IconWrapper>
+            <PiShrimpFill size={40} color={"aliceblue"} />
+          </IconWrapper>
+          <div>
+            <LabelText>Quantidade de Animais</LabelText>
+            <StyledInput
+              placeholder="Calculado automaticamente"
+              type="number"
+              value={quantidadeAnimais}
+              readOnly
+            />
+          </div>
+        </StyledBox>
 
-            <Box
-              bg={"#064F57"}
-              w={"290px"}
-              h={"94px"}
-              color={"aliceblue"} 
-              rounded="md" 
-              boxShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}
-              position={"relative"}
-              
-              
-              display={"flex"}
-              >
-                <Box
-                  ml={"30px"}
-                  mt={"25px"}
-                  mr={"40px"}
-                  >
-                  
-                    <FaCalculator size={40} color={"aliceblue"} />
-                </Box>
+        {/* Quantidade de Ração */}
+        <StyledBox>
+          <IconWrapper>
+            <GiFlour size={40} color={"aliceblue"} />
+          </IconWrapper>
+          <div>
+            <LabelText>Quantidade de Ração (kg)</LabelText>
+            <StyledInput
+              placeholder="Calculado automaticamente"
+              type="number"
+              value={quantidadeRacao}
+              readOnly
+            />
+          </div>
+        </StyledBox>
 
-                <Box>
-                <Text
-                position={"absolute"}
-                top={"-55%"}
-                left={"-27%"}
-                transform={"translate(64%, 50%)"}
-                  
-                  
-                >
-                  FCA estimado
-                </Text>
-                
-                <Input
-                  placeholder="Digite aqui"
-                  border={"none"}
-                  step="0.01"
-                  type="number"
-                  value={fcaEstimado}
-                  onChange={(e) => handleInputChange('fcaEstimado', e.target.value)}
-                  _focus={{ outline: "none", borderColor: "none" }}
-                  mt={"20px"}
-                />
-                </Box> 
+        {/* Quantidade de Sacas */}
+        <StyledBox>
+          <IconWrapper>
+            <GiWeight size={40} color={"aliceblue"} />
+          </IconWrapper>
+          <div>
+            <LabelText>Quantidade de Sacas (25kg)</LabelText>
+            <StyledInput
+              placeholder="Calculado automaticamente"
+              type="number"
+              value={quantidadeSacas}
+              readOnly
+            />
+          </div>
+        </StyledBox>
 
-                
-                
-               
-                
-            </Box>
+        {/* Densidade */}
+        <StyledBox>
+          <IconWrapper>
+            <FaClock size={40} color={"aliceblue"} />
+          </IconWrapper>
+          <div>
+            <LabelText>Densidade (animais/m²)</LabelText>
+            <StyledInput
+              placeholder="Calculado automaticamente"
+              type="number"
+              value={densidade}
+              readOnly
+            />
+          </div>
+        </StyledBox>
+      </div>
+    </>
+  );
+};
 
-            <Box
-              bg={"#064F57"}
-              w={"290px"}
-              h={"94px"}
-              color={"aliceblue"} 
-              rounded="md" 
-              boxShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}
-              position={"relative"}
-              
-              display={"flex"}
-              >
-                <Box
-                  ml={"30px"}
-                  mt={"25px"}
-                  mr={"40px"}
-                  >
-                  
-                    <GiWeight size={40} color={"aliceblue"} />
-                </Box>
-
-                <Box>
-                <Text
-                position={"absolute"}
-                top={"-55%"}
-                left={"-41%"}
-                transform={"translate(64%, 50%)"}
-                  
-                  
-                >
-                  Quantidade de Ração
-                </Text>
-                
-                <Input
-                   placeholder={quantidadeRacao ? quantidadeRacao.toFixed(2) : ""}
-                  border={"none"}
-                  type="number"
-                  readOnly
-                  _placeholder={{
-                    color: "white", 
-                  }}
-                  
-                  _focus={{ outline: "none", borderColor: "none" }}
-                  mt={"20px"}
-                />
-                </Box> 
-
-                
-                
-               
-                
-            </Box>
-
-
-
-            <Box
-              bg={"#064F57"}
-              w={"290px"}
-              h={"94px"}
-              color={"aliceblue"} 
-              rounded="md" 
-              boxShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}
-              position={"relative"}
-              
-              display={"flex"}
-              >
-                <Box
-                  ml={"30px"}
-                  mt={"25px"}
-                  mr={"40px"}
-                  >
-                  
-                    <GiFlour size={40} color={"aliceblue"} />
-                </Box>
-
-                <Box>
-                <Text
-                position={"absolute"}
-                top={"-55%"}
-                left={"-60%"}
-                transform={"translate(64%, 50%)"}
-                  
-                  
-                >
-                  Quantidade de sacas de Ração
-                </Text>
-                
-                <Input
-                  placeholder={quantidadeSacas? quantidadeSacas.toFixed(0) : ""}
-                  border={"none"}
-                  type="number"
-                  readOnly
-                  _placeholder={{
-                    color: "white", 
-                  }}
-                  _focus={{ outline: "none", borderColor: "none" }}
-                  mt={"20px"}
-                />
-                </Box> 
-
-                
-                
-               
-                
-            </Box>
-
-            <Box
-              bg={"#064F57"}
-              w={"290px"}
-              h={"94px"}
-              color={"aliceblue"} 
-              rounded="md" 
-              boxShadow={"0px 4px 4px rgba(0, 0, 0, 0.25)"}
-              position={"relative"}
-              
-              
-              display={"flex"}
-              >
-                <Box
-                  ml={"30px"}
-                  mt={"25px"}
-                  mr={"40px"}
-                  >
-                  
-                    <FaClock size={40} color={"aliceblue"} />
-                </Box>
-
-                <Box>
-                <Text
-                position={"absolute"}
-                top={"-55%"}
-                left={"-41%"}
-                transform={"translate(64%, 50%)"}
-                  
-                  
-                >
-                  Densidade de Cultivo 
-                </Text>
-                
-                <Input
-                  placeholder={densidade ? Math.ceil(densidade).toString() : ""}
-                  border={"none"}
-                  type="number"
-                  readOnly
-                  value={densidade ? Math.ceil(densidade) : ""}
-                  _focus={{ outline: "none", borderColor: "none" }}
-                  mt={"20px"}
-                />
-
-                </Box> 
-
-                
-                
-               
-                
-            </Box>
-
-
-
-          </Flex>
-        
-        </>
-     );
-}
- 
 export default Planejamento;
