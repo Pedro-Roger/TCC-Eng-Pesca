9ba8465a8dd736254e3d81b2cbfc572c37aae4d2
Pedro-Roger <eng.pedrodev@gmail.com>
2025-03-18 23:22:12 -0300

    fix side bar

--- src/components/my components/sidebar.tsx
+++ src/components/my components/sidebar.tsx
@@ -1,6 +1,6 @@
-import { Button, VStack } from "@chakra-ui/react";
-import { FiPlus } from "react-icons/fi";
-import { useNavigate } from "react-router-dom";
+import { Button, VStack } from '@chakra-ui/react';
+import { FiPlus } from 'react-icons/fi';
+import { useNavigate } from 'react-router-dom';
 
 const SideBar = () => {
   const navigate = useNavigate();
@@ -17,18 +17,15 @@ const SideBar = () => {
         bg="none"
         color="aliceblue"
         fontSize="20px"
-        mt={"10px"}
+        mt={'10px'}
         justifyContent="flex-start"
         width="100%"
-        onClick={onClick} 
-        _hover={
-          {
-            bg: "#808080",
-            border: "1px solid aliceblue",
-            color: "aliceblue",
-          }
-          
-        }
+        onClick={onClick}
+        _hover={{
+          bg: '#808080',
+          border: '1px solid aliceblue',
+          color: 'aliceblue',
+        }}
       >
         {children}
       </Button>
@@ -38,42 +35,42 @@ const SideBar = () => {
   return (
     <>
       <VStack
-        display={{ base: "none", md: "block" }}
-        color={"aliceblue"}
+        display={{ base: 'none', md: 'block' }}
+        color={'aliceblue'}
         bg="#00000d"
-        w={"279px"}
-        h={{ base: "100vh", md: "auto" }}
-        padding={"20px"}
-        gap={"20px"}
+        w={'279px'}
+        h={{ base: '100vh', md: '100vh' }}
+        padding={'20px'}
+        gap={'20px'}
         overflowY="auto"
       >
         <Button
-          bg={"none"}
-          color={"aliceblue"}
-          fontSize={"20px"}
-          border={"1px solid aliceblue"}
-          borderRadius={"0px"}
-          mb={"20px"}
-          mt={"20px"}
-          onClick={() => navigate("/")}
+          bg={'none'}
+          color={'aliceblue'}
+          fontSize={'20px'}
+          border={'1px solid aliceblue'}
+          borderRadius={'0px'}
+          mb={'20px'}
+          mt={'20px'}
+          onClick={() => navigate('/')}
         >
           Novo Projeto
           <FiPlus />
         </Button>
 
-        <SidebarButton onClick={() => navigate("/ListaProjetos")}>
+        <SidebarButton onClick={() => navigate('/ListaProjetos')}>
           Lista de projetos
         </SidebarButton>
         <SidebarButton>Em Andamento</SidebarButton>
-        <SidebarButton
-          onClick={() => navigate("/TabelaArracoamento")}
-        >Tabela de Arraçoamento</SidebarButton>
-        <SidebarButton
-          onClick={() => navigate("/Biometria")}
-        >Biometria</SidebarButton>
-        <SidebarButton
-          onClick={() => navigate("/Equipe")}
-        >Equipe</SidebarButton>
+        <SidebarButton onClick={() => navigate('/TabelaArracoamento')}>
+          Tabela de Arraçoamento
+        </SidebarButton>
+        <SidebarButton onClick={() => navigate('/Biometria')}>
+          Biometria
+        </SidebarButton>
+        <SidebarButton onClick={() => navigate('/Equipe')}>
+          Equipe
+        </SidebarButton>
       </VStack>
     </>
   );
